f276f67783e4e472bc58c0d727dbf68dfb9c1d76
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-24 22:15:03 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,4 +1,5 @@
 # since we are not using ActiveRecord in this lab, you write a bare Ruby model (like you did in MOD1)
+
 class Puppy
   attr_accessor :name, :breed, :age 
   
@@ -7,7 +8,4 @@ class Puppy
     @breed = breed 
     @age = age 
   end 
-  
-  
-  
 end 
